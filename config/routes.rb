81c7ddd0a4a594f1f8a5fa44ccae84e390e81ee5
Ricardo Marques <ricardo.ds.marques@gmail.com>
2018-04-18 01:57:52 +0100

    dynamic route fetching

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    # dynamically fetching routes from views folder
+    Dir.glob('app/views/admin/*').select { |f| File.directory? f }.map { |directory| directory.split('/').last.to_sym }.each do |resources|
+      resources resources
+    end
+  end
 end
